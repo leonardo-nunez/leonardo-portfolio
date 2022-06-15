58784fdd7250283db48ebd5d201ede10da7a98d1
Leo <lnpb@hotmail.com>
2022-06-15 11:16:06 +0200

    Navigation working

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,51 @@
+import { useState } from 'react';
 import { Link } from 'react-scroll';
 
 const Navbar = () => {
+  const [navActive, setNavActive] = useState(false);
+
+  const onClickHandler = (e) => {
+    e.preventDefault();
+    setNavActive((active) => !active);
+  };
+
   return (
-    <nav className="nav">
-      <ul className="nav__ul">
-        <Link to="intro">
-          <li className="nav__li">HOME</li>
-        </Link>
-        <Link to="about" offset={-145}>
-          <li className="nav__li">ABOUT</li>
-        </Link>
-        <Link to="skills" offset={80}>
-          <li className="nav__li">SKILLS</li>
-        </Link>
-        <Link to="projects" offset={-110}>
-          <li className="nav__li">PROJECTS</li>
-        </Link>
-        <Link to="contact">
-          <li className="nav__li">CONTACT</li>
-        </Link>
-      </ul>
-    </nav>
+    <div className="screen">
+      <div className={`${navActive ? 'nav--active' : ''}`}>
+        <div className="nav__trigger" onClick={onClickHandler}>
+          <span className="nav__icon" />
+        </div>
+        <nav className="nav">
+          <ul className="nav__list">
+            <Link to="intro">
+              <li className="nav__item" onClick={onClickHandler}>
+                HOME
+              </li>
+            </Link>
+            <Link to="about" offset={-145}>
+              <li className="nav__item" onClick={onClickHandler}>
+                ABOUT
+              </li>
+            </Link>
+            <Link to="skills" offset={80}>
+              <li className="nav__item" onClick={onClickHandler}>
+                SKILLS
+              </li>
+            </Link>
+            <Link to="projects" offset={-110}>
+              <li className="nav__item" onClick={onClickHandler}>
+                PROJECTS
+              </li>
+            </Link>
+            <Link to="contact">
+              <li className="nav__item" onClick={onClickHandler}>
+                CONTACT
+              </li>
+            </Link>
+          </ul>
+        </nav>
+      </div>
+    </div>
   );
 };
 
