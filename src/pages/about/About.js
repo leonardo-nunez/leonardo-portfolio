3ae936e1d979d0890f8c4798e1c986e39066d03e
Leo <lnpb@hotmail.com>
2022-06-28 16:18:27 +0200

    centered about text

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -7,14 +7,16 @@ const About = () => {
       <article className="about__wrapper">
         <h2 className="about__title">About</h2>
         <article className="about__content">
-          <p className="about__text">
-            Full-stack web developer, video editor and show performer. <br />
-            In 2021 I decided to make a switch from the stage into coding. I
-            applied to
-            <span className="highlighted"> &lt;/SALT&gt;</span> and was one of
-            the 40 candidates that got in from over 1000 applicants.
-            <br />I love problem solving, being creative and working in teams.
-          </p>
+          <div className="about__text">
+            <div>
+              Full-stack web developer, video editor and show performer. <br />
+              In 2021 I decided to make a switch from the stage into coding. I
+              applied to
+              <span className="highlighted"> &lt;/SALT&gt;</span> and was one of
+              the 40 candidates that got in from over 1000 applicants.
+              <br />I love problem solving, being creative and working in teams.
+            </div>
+          </div>
           <figure className="about__image-wrapper">
             <img
               className="about__images about__images--cats"
