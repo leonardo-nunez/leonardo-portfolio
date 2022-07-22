d3435e7765f7acb114590ba7eded7f627211e271
Leo <lnpb@hotmail.com>
2022-07-22 13:32:37 +0200

    update skills

--- src/pages/skills/Skills.js
+++ src/pages/skills/Skills.js
@@ -24,7 +24,9 @@ const Skills = () => {
           </article>
           <article className="skills__box skills__box--frontend">
             <h3>Frontend</h3>
-            <p>React, React Native, Angular, Redux, HTML, CSS, Figma, UX</p>
+            <p>
+              React, React Native, NextJS, Angular, Redux, HTML, CSS, Figma, UX
+            </p>
           </article>
           <article className="skills__box skills__box--tools">
             <h3>Tools and technologies</h3>
