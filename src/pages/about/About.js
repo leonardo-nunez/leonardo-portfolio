88fd84e87fc73106fd6aa3e45a3ca6560a302cff
Leonardo Nunez <lnpb@hotmail.com>
2022-11-17 12:05:00 +0100

    Update about, add CV

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,32 +1,44 @@
-import CATS from './images/CATS.jpg';
-import Circus from './images/Circus.jpg';
+import CATS from "./images/CATS.jpg";
+import Circus from "./images/Circus.jpg";
 
 const About = () => {
   return (
-    <section className="about">
-      <article className="about__wrapper">
-        <h2 className="about__title">About</h2>
-        <article className="about__content">
-          <div className="about__text">
+    <section className='about'>
+      <article className='about__wrapper'>
+        <h2 className='about__title'>About</h2>
+        <article className='about__content'>
+          <div className='about__text'>
             <div>
-              Full-stack web developer, video editor and show performer. <br />
+              I'm a full-stack web developer, video editor and show performer.{" "}
+              <br />
               In 2021 I decided to make a switch from the stage into coding. I
               applied to
-              <span className="highlighted"> &lt;/SALT&gt;</span> and was one of
+              <span className='highlighted'> &lt;/SALT&gt;</span> and was one of
               the 40 candidates that got in from over 1000 applicants.
               <br />I love problem solving, being creative and working in teams.
+              <br />
+              I speak English, Swedish and Spanish fluently.
+              <br />
+              My full CV can be found{" "}
+              <a
+                className='about__cv-link'
+                href='/assets/files/cv.pdf'
+              >
+                here
+              </a>
+              .
             </div>
           </div>
-          <figure className="about__image-wrapper">
+          <figure className='about__image-wrapper'>
             <img
-              className="about__images about__images--cats"
+              className='about__images about__images--cats'
               src={CATS}
-              alt="CATS"
+              alt='CATS'
             />
             <img
-              className="about__images about__images--circus"
+              className='about__images about__images--circus'
               src={Circus}
-              alt="Circus"
+              alt='Circus'
             />
           </figure>
         </article>
--- src/pages/projects/Project.js
+++ src/pages/projects/Project.js
@@ -1,40 +1,40 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import { VerticalTimelineElement } from 'react-vertical-timeline-component';
-import styles from './assets/VerticalTimelineStyles';
+import React from "react";
+import Button from "react-bootstrap/Button";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import { VerticalTimelineElement } from "react-vertical-timeline-component";
+import styles from "./assets/VerticalTimelineStyles";
 
 const Project = ({ background, demoLink, icon, img, url, title, text }) => {
   return (
     <VerticalTimelineElement
       className={`project project__${background}`}
-      contentStyle={background === 'dark' ? styles.dark : styles.light}
+      contentStyle={background === "dark" ? styles.dark : styles.light}
       contentArrowStyle={
-        background === 'dark'
-          ? { borderRight: '8px solid  #f9f9f9' }
-          : { borderRight: '8px solid  #3a5d41' }
+        background === "dark"
+          ? { borderRight: "8px solid  #f9f9f9" }
+          : { borderRight: "8px solid  #3a5d41" }
       }
       iconStyle={styles.icon}
-      icon={<span className="material-symbols-outlined">{icon}</span>}
+      icon={<span className='material-symbols-outlined'>{icon}</span>}
     >
-      <div className="project__wrapper">
-        <div className="project__text">
-          <h3 className="project__title">{title}</h3>
+      <div className='project__wrapper'>
+        <div className='project__text'>
+          <h3 className='project__title'>{title}</h3>
           <p>{text}</p>
-          <div className="project__buttons-wrapper">
+          <div className='project__buttons-wrapper'>
             <a
-              className="project__link"
+              className='project__link'
               href={url}
-              target="_blank"
-              rel="noreferrer"
+              target='_blank'
+              rel='noreferrer'
             >
               <Button
                 className={
-                  background === 'light'
-                    ? 'project__button project__button-light project__button--github'
-                    : 'project__button'
+                  background === "light"
+                    ? "project__button project__button-light project__button--github"
+                    : "project__button"
                 }
-                variant="success"
+                variant='success'
               >
                 {<GitHubIcon />} GitHub
               </Button>
@@ -42,32 +42,36 @@ const Project = ({ background, demoLink, icon, img, url, title, text }) => {
             &nbsp;
             {demoLink && (
               <a
-                className="project__link"
+                className='project__link'
                 href={demoLink}
-                target="_blank"
-                rel="noreferrer"
+                target='_blank'
+                rel='noreferrer'
               >
                 <Button
                   className={
-                    background === 'light'
-                      ? 'project__button project__button-demo project__button-light'
-                      : 'project__button project__button-demo'
+                    background === "light"
+                      ? "project__button project__button-demo project__button-light"
+                      : "project__button project__button-demo"
                   }
-                  variant="success"
+                  variant='success'
                 >
-                  <span className="material-symbols-outlined">laptop_mac</span>
+                  <span className='material-symbols-outlined'>laptop_mac</span>
                   &nbsp;Demo
                 </Button>
               </a>
             )}
           </div>
         </div>
-        <a href={demoLink ? demoLink : url} target="_blank" rel="noreferrer">
-          <button className="project__img-btn">
+        <a
+          href={demoLink ? demoLink : url}
+          target='_blank'
+          rel='noreferrer'
+        >
+          <button className='project__img-btn'>
             <img
-              className="project__img"
-              src={`/project_images/${img}`}
-              alt="projImage"
+              className='project__img'
+              src={`/assets/project_images/${img}`}
+              alt='projImage'
             />
           </button>
         </a>
