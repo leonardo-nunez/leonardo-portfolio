063d51902c27d292881bfa2526d3525de5aa44f8
Leonardo Nunez <lnpb@hotmail.com>
2022-11-17 11:38:19 +0100

    add phone number and new intro text

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,32 +1,52 @@
-import EmailIcon from '@mui/icons-material/Email';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import EmailIcon from "@mui/icons-material/Email";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import PhoneIcon from "@mui/icons-material/Phone";
 
 const Contact = () => {
   return (
-    <section className="contact">
-      <h2 className="contact__title">Contact</h2>
-      <div className="contact__icons-wrapper">
+    <section className='contact'>
+      <h2 className='contact__title'>Contact</h2>
+      <div className='contact__icons-wrapper'>
         <a
-          href="mailto:leonardo.nunez123@gmail.com"
-          target="_blank"
-          rel="noreferrer"
+          href='mailto:leonardo.nunez123@gmail.com'
+          target='_blank'
+          rel='noreferrer'
         >
-          <EmailIcon className="contact__icon" style={{ fill: 'black' }} />
+          <EmailIcon
+            className='contact__icon'
+            style={{ fill: "black" }}
+          />
         </a>
         <a
-          href="https://github.com/leonardo-nunez"
-          target="_blank"
-          rel="noreferrer"
+          href='https://github.com/leonardo-nunez'
+          target='_blank'
+          rel='noreferrer'
         >
-          <GitHubIcon className="contact__icon" style={{ fill: 'black' }} />
+          <GitHubIcon
+            className='contact__icon'
+            style={{ fill: "black" }}
+          />
         </a>
         <a
-          href="https://www.linkedin.com/in/leonardo-nunez-del-prado/"
-          target="_blank"
-          rel="noreferrer"
+          href='https://www.linkedin.com/in/leonardo-nunez-del-prado/'
+          target='_blank'
+          rel='noreferrer'
         >
-          <LinkedInIcon className="contact__icon" style={{ fill: 'black' }} />
+          <LinkedInIcon
+            className='contact__icon'
+            style={{ fill: "black" }}
+          />
+        </a>
+        <a
+          href='tel:+46736284123'
+          target='_blank'
+          rel='noreferrer'
+        >
+          <PhoneIcon
+            className='contact__icon'
+            style={{ fill: "black" }}
+          />
         </a>
       </div>
     </section>
--- src/pages/intro/Intro.js
+++ src/pages/intro/Intro.js
@@ -1,25 +1,29 @@
-import leoPortrait from './images/leo-portrait.jpg';
-import tropicalLeaf from '../../images/tropical-leaf-silhouette.png';
+import leoPortrait from "./images/leo-portrait.jpg";
+import tropicalLeaf from "../../images/tropical-leaf-silhouette.png";
 
 const Intro = () => {
   return (
-    <section className="intro">
-      <div className="intro__background" />
+    <section className='intro'>
+      <div className='intro__background' />
       <img
-        className="intro__tropical--left"
+        className='intro__tropical--left'
         src={tropicalLeaf}
-        alt="background_image"
+        alt='background_image'
       />
-      <div className="intro__wrapper">
-        <article className="intro__text">
+      <div className='intro__wrapper'>
+        <article className='intro__text'>
           <h1>
-            Hello I'm <span className="highlighted">Leonardo Nunez</span>
+            Hello I'm <span className='highlighted'>Leonardo Nunez</span>
           </h1>
-          <h3>a full-stack web developer</h3>
+          <h3>a web developer</h3>
         </article>
-        <img className="intro__image" src={leoPortrait} alt="leo-portrait" />
+        <img
+          className='intro__image'
+          src={leoPortrait}
+          alt='leo-portrait'
+        />
       </div>
-      <div className="vertical-dots" />
+      <div className='vertical-dots' />
     </section>
   );
 };
